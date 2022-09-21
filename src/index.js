37203514fc1e5da295c732396a43cac50e7a627a
pjaeyoung <devdodose@gmail.com>
2022-09-21 23:32:01 +0900

    feat: shape animation 구현

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const COLORS = ["#dab7a3", "#d39b75", "#9ca18d", "#9ca18d", "#21574a"];
+const WINDOW_HEIGHT = window.innerHeight;
+const WINDOW_WIDTH = window.innerWidth;
+
+const $colorName = document.getElementById("selected-color-name");
+
+changeColor(COLORS[0]);
+createPalette();
+createShapes();
+
+function changeColor(selectedColor) {
+  $colorName.textContent = selectedColor;
+  document.body.style.backgroundColor = selectedColor;
+}
+
+function createPalette() {
+  const $palette = document.getElementById("palette");
+  const fragment = document.createDocumentFragment();
+  COLORS.forEach((colorName) => {
+    fragment.appendChild(createPaletteColor(colorName));
+  });
+
+  $palette.appendChild(fragment);
+}
+
+function createPaletteColor(colorName) {
+  const $li = document.createElement("li");
+  const $button = document.createElement("button");
+  $button.className = "color";
+  $button.dataset.color = colorName;
+  $button.style.backgroundColor = colorName;
+
+  $li.appendChild($button);
+
+  return $li;
+}
+
+function createShapes() {
+  const $shapes = document.getElementById("shapes");
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < 140; i++) {
+    fragment.appendChild(createShape());
+  }
+
+  $shapes.appendChild(fragment);
+}
+
+function createShape() {
+  const $li = document.createElement("li");
+  $li.className = "shape";
+  $li.style.backgroundColor = COLORS[0];
+  $li.style.top = `${getRandomNumber(-10, WINDOW_HEIGHT)}px`;
+  $li.style.left = `${getRandomNumber(-10, WINDOW_WIDTH)}px`;
+  runAnimation($li);
+  return $li;
+}
+
+function runAnimation($shape) {
+  const scale = getRandomNumber(0.8, 2);
+  const posX = getRandomNumber(-500, 800);
+  const posY = getRandomNumber(-400, 800);
+  $shape.style.transform = `scale(${scale}) translate(${posX}px, ${posY}px)`;
+  setTimeout(() => runAnimation($shape), 2000);
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
